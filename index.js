e30e48cbe7ba4f348eaaf306f4b448cd339a75a9
Xylops <rankine1607@gmail.com>
2019-08-08 23:57:45 +0800

    adding express basic

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Working')
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.port || 3000;
+// const url = 'https://api.telegram.org/bot';
+const token = process.env.TelegramToken;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
