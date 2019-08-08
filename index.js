d09a0a7e7d9eba02179ae790c97c5d80619e6952
Xylops <rankine1607@gmail.com>
2019-08-09 00:08:27 +0800

    adding node telegram

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-const port = process.env.port || 3000;
-// const url = 'https://api.telegram.org/bot';
+const TelegramBot = require('node-telegram-bot-api');
+
+// const port = process.env.port || 3000;
 const token = process.env.TelegramToken;
+const bot = new TelegramBot(token, {polling: true});
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// app.listen(port, () => {
+//     console.log(`Listening on port ${port}`);
+// });
+
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg)
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message : ' + msg.text);
 });
