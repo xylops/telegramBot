7fe2a76be916469d7710e3d24a8e99fce4fdb2c6
Xylops <rankine1607@gmail.com>
2019-08-14 00:59:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const PORT = parseInt(process.env.PORT) || 3000
 service.connectMongoose();
 
 app.use(express.static('./public')); 
-app.use((req, res, next) => {
-    if(req.headers['x-forwarded-proto'] === 'https'){
-      res.redirect('http://' + req.hostname + req.url);
-    }else{
-        next();
-    }
-})
+// app.use((req, res, next) => {
+//     if(req.headers['x-forwarded-proto'] === 'https'){
+//       res.redirect('http://' + req.hostname + req.url);
+//     }else{
+//         next();
+//     }
+// })
 require('./controller')(app)
 app.listen(PORT, () => {
     console.log('Express server is up on port ' + PORT )
